56d4212f22b007ae5bbcbe030c526e77d84638bd
Micheal Winger <mike.winger87@gmail.com>
2017-12-04 18:20:16 -0500

    Project Start with moving box.
    Next up is to add some AI, and maybe restrain the movement some.

---
+++ Assets/Scripts/BoxControllerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxControllerScript : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    [Range(1, 10)]
+    public int jumpVelocity = 5;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity += Vector2.up * jumpVelocity;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
